2a6a99d5d4645aa9c149b0e6d2a8e134cc33ff90
0dsec <0dze@pm.me>
2026-04-30 20:11:34 -0400

    ascii art update

--- ytmp3.py
+++ ytmp3.py
@@ -1,10 +1,49 @@
 #!/usr/bin/env python3
+
 import sys
 import shutil
 import os
 import subprocess
 from typing import Any
 import yt_dlp
+
+BLUE = "\033[38;5;117m"  # pastel blue
+RED = "\033[38;5;203m"  # pastel red
+RESET = "\033[0m"
+
+YT = [
+    "oooooo   oooo ooooooooooooo ",
+    " `888.   .8'  8'   888   `8 ",
+    "  `888. .8'        888      ",
+    "   `888.8'         888      ",
+    "    `888'          888      ",
+    "     888           888      ",
+    "    o888o         o888o     ",
+    "                            ",
+    "                            ",
+]
+
+RIPPER = [
+    "ooooooooo.    o8o                                           ",
+    "`888   `Y88.  `\"'                                           ",
+    " 888   .d88' oooo  oo.ooooo.  oo.ooooo.   .ooooo.  oooo d8b ",
+    " 888ooo88P'  `888   888' `88b  888' `88b d88' `88b `888\"\"8P ",
+    " 888`88b.     888   888   888  888   888 888ooo888  888     ",
+    " 888  `88b.   888   888   888  888   888 888    .o  888     ",
+    "o888o  o888o o888o  888bod8P'  888bod8P' `Y8bod8P' d888b    ",
+    "                    888        888                          ",
+    "                   o888o      o888o                         ",
+]
+
+
+def clear_terminal():
+    os.system("clear")
+
+
+def show_banner():
+    for left, right in zip(YT, RIPPER):
+        print(BLUE + left + RED + right + RESET)
+    print()
 
 
 def check_ffmpeg():
@@ -53,6 +92,9 @@ def open_folder(folder):
 
 
 def main():
+    clear_terminal()
+    show_banner()
+
     if len(sys.argv) < 2:
         print("Usage:")
         print("  ytaudio URL [output_folder]")
@@ -68,7 +110,7 @@ def main():
 
     saved_file = download_audio(url, output_folder)
 
-    print("Done.")
+    print("\nDone.")
     print(f"Saved to: {saved_file}")
 
     open_folder(output_folder)
